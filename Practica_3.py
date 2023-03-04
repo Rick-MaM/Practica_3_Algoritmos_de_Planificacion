6959e67e1379dfde1a34ffc33fdf1089f4b622b1
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-03-04 14:22:04 -0600

    Se agrego la funcion File

--- Practica_3.py
+++ Practica_3.py
@@ -1,1 +1,18 @@
 import time 
+
+class planning:
+
+    def __init__(self):
+        pass
+
+    def File(self):
+        with open("procesos.txt","r") as file:
+            file = file.readlines()
+        return file
+    
+    
+    
+
+proceso = planning()
+file = proceso.File()
+print(file[1])

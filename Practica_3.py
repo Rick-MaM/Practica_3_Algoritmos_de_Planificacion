98e919c56a052ace2a6f96ae37d693e6a02e8631
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-03-04 18:56:58 -0600

    Se elimino la ventana

--- Practica_3.py
+++ Practica_3.py
@@ -3,18 +3,11 @@ from tkinter import *
 
 class planning:
 
-    def __init__(self,windows):
-        self.Windows = windows
-        self.Windows.title("Algoritmo de Planificacion")
-        self.Windows.geometry("400x400")
+    def __init__(self):
         self.File()
 
     def File(self):
         with open("procesos.txt","r") as self.file:
             self.file = self.file.readlines()
-
     
-if __name__=="__main__":
-    root=Tk()
-    aplicacion = planning(root)
-    root.mainloop()
+

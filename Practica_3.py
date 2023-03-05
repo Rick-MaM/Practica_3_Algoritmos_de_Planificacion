3b1280caef8aaa6ca8e093f799c61670e2666030
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-03-04 19:11:38 -0600

    Se agrego la funcion FIF

--- Practica_3.py
+++ Practica_3.py
@@ -10,4 +10,15 @@ class planning:
         with open("procesos.txt","r") as self.file:
             self.file = self.file.readlines()
     
+    def FIFO(self):
+        while len(self.file) != 0:
+            process = self.file[0].split(",")
+            print("Proceso: ", process[0], "------> ",end="")
+            for count in range(int(process[2])):
+                print(".", end="")
+                time.sleep(1)
+            print("Completado")
+            self.file.pop(0)
 
+proceso = planning()
+proceso.FIFO()

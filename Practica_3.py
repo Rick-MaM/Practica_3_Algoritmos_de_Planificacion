95a2fcd90ee11b0b7154b3be98d3f064fa483e05
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-03-07 13:07:15 -0600

    Se agrego la funcion SJF

--- Practica_3.py
+++ Practica_3.py
@@ -8,7 +8,16 @@ class Process:
     def File(self):
         with open("procesos.txt","r") as self.file:
             self.file = self.file.readlines()
-    
+
+    def SJF(self):
+        for count_process in range (len(self.file)):
+            for count_time in range (len(self.file)):
+                process = self.file[count_time].split(",")
+                if int(process[2]) == count_process:
+                    time.sleep(int(process[2]))
+                    print("Proceso: ", process[0],
+                          "------> Completado", "------> Tiempo: ",int(process[2]))
+            
     def FIFO(self):
         aux_file = self.file
         while len(aux_file) != 0:
@@ -39,4 +48,4 @@ class Process:
             count_process += 1
 
 proceso = Process()
-proceso.Priority()
+proceso.SJF()

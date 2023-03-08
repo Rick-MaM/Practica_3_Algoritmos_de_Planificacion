641e3eb4625a3ee41185d3bc85e7269620457819
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-03-08 11:47:26 -0600

    Se quitaron los aux_file

--- Practica_3.py
+++ Practica_3.py
@@ -11,9 +11,9 @@ class Process:
     
     def Round_Robin(self):
         quantum = 3
-        file = self.file
-        while len(file) != 0:
-            process = file[0].split(",")
+        self.File()
+        while len(self.file) != 0:
+            process = self.file[0].split(",")
             time_process = int(process[2])
             print("Proceso: ", process[0], "----- ->",end="")
             for count_time in range(quantum):
@@ -25,10 +25,10 @@ class Process:
                 print(".",end="")
             if time_process > 0:
                 print("Proceso: ", process[0], "------> ",time_process)
-                file.append(process[0]+", "+process[1]+", "+str(time_process))
+                self.file.append(process[0]+", "+process[1]+", "+str(time_process))
             else:
                 print("Completado")
-            file.pop(0)
+            self.file.pop(0)
 
     def shoter_time(self,list_SJF):
         Time = []
@@ -39,12 +39,12 @@ class Process:
         return Time
 
     def SJF(self):
-        aux_file = self.file
-        times = self.shoter_time(aux_file)
-        number_process = len(aux_file)
-        while len(aux_file) != 0:
+        self.File()
+        times = self.shoter_time(self.file)
+        number_process = len(self.file)
+        while len(self.file) != 0:
             for count_process in range(number_process+1):
-                process = aux_file[count_process].split(",")
+                process = self.file[count_process].split(",")
                 if times[0] == int(process[2]):
                     print("Proceso: ",process[0], "------> ",end="")
                     for count in range(times[0]):
@@ -52,20 +52,20 @@ class Process:
                         print(".",end="")
                     print("completado")
                     break
-            aux_file.pop(count_process)
-            number_process = len(aux_file)
+            self.file.pop(count_process)
+            number_process = len(self.file)
             times.pop(0)
   
     def FIFO(self):
-        aux_file = self.file
-        while len(aux_file) != 0:
-            process = aux_file[0].split(",")
+        self.Fiel()
+        while len(self.file) != 0:
+            process = self.file[0].split(",")
             print("Proceso: ", process[0], "------> ",end="")
             for count in range(int(process[2])):
                 print(".", end="")
                 time.sleep(1)
             print("Completado")
-            aux_file.pop(0)
+            self.file.pop(0)
     
     def process_priority(self,list_priority):
         number_priority = []
@@ -76,12 +76,12 @@ class Process:
         return number_priority
     
     def priority(self):
-        aux_file = self.file
-        list_priority = self.process_priority(aux_file)
-        number_process = len(aux_file)
-        while len(aux_file) != 0:
+        self.File()
+        list_priority = self.process_priority(self.file)
+        number_process = len(self.file)
+        while len(self.file) != 0:
             for count_process in range(number_process+1):
-                process = aux_file[count_process].split(",")
+                process = self.file[count_process].split(",")
                 if list_priority[0] == int(process[1]):
                     print("Proceso: ", process[0], "------> ", end="")
                     for count in range(int(process[2])):
@@ -89,18 +89,17 @@ class Process:
                         print(".", end="")
                     print("completado")
                     break
-            aux_file.pop(count_process)
-            number_process = len(aux_file)
+            self.file.pop(count_process)
+            number_process = len(self.file)
             list_priority.pop(0)
 
 proceso = Process()
-"""
+
 print("-----> Round Robin <-----")
 proceso.Round_Robin()
 print("-----> SJF <-----")
 proceso.SJF()
 print("-----> FIFO <-----")
 proceso.FIFO()
-"""
 print("-----> Prioridad <-----")
 proceso.priority()
